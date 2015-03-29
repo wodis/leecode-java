d95a02184b901e7e9c33017ed12c2c73cb0a615c
wodis <wodis1988@gmail.com>
2015-03-29 10:23:43 +0800

    consume time

--- src/java/algorithms/App.java
+++ src/java/algorithms/App.java
@@ -2,12 +2,17 @@ package algorithms;
 
 import algorithms.t38.CountAndSay;
 
+import java.util.Date;
+
 /**
  * Created by wudi on 2015/3/29.
  */
 public class App {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         CountAndSay countAndSay = new CountAndSay();
-        System.out.println(countAndSay.countAndSay(8));
+        long start = new Date().getTime();
+        System.out.println(countAndSay.countAndSay(5));
+        long end = new Date().getTime();
+        System.out.println(end - start);
     }
 }

9edb0b38c1acd0243320dad8c87cb3f99b9c25e3
wudi <di.wu@yolu-inc.com>
2015-03-30 09:23:36 +0800

    create t38 Pascal's Triangle

---
+++ src/java/algorithms/t118/PascalsTriangle.java
@@ -0,0 +1,20 @@
+package algorithms.t118;
+
+/**
+ * Given numRows, generate the first numRows of Pascal's triangle.
+ * <p/>
+ * For example, given numRows = 5,
+ * Return
+ * <p/>
+ * [
+ * [1],
+ * [1,1],
+ * [1,2,1],
+ * [1,3,3,1],
+ * [1,4,6,4,1]
+ * ]
+ * <p/>
+ * Created by wudi on 15-3-30.
+ */
+public class PascalsTriangle {
+}
